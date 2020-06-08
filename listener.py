978d440b2799dc573994a0340fc260609ddd1cd3
AutonomousHansen <halexhansen@gmail.com>
2020-06-08 10:08:06 -0400

    Updated listener to be classier, handle incoming messages more sanely

--- listener.py
+++ listener.py
@@ -3,37 +3,52 @@
 
 from pushover_open_client import Client
 
-def messageCallback(messageList):
-#Prcoess/do work with messageList!
-    if(messageList):
-        for message in messageList:
-            #Do work with message here!
-            print("#{}\n{}\n{}".format(message.id, message.title, message.message))
-            if "EMERGENCY" in message.message:
-                print('Emergency shut off initiated')
-                print('Shutting off the arduino now...')
-                print('Shut off completed')
+class ReyClient(object):
 
-            #Make sure to acknowledge messages with priority >= 2
-            if(message.priority >= 2):
-                if(message.acked != 1):
-                    client.acknowledgeEmergency(message.receipt)	
-        #Make sure you delete messages that you recieve!
-        client.deleteMessages(messageList[-1].id)
+    def __init__(self, client):
+        self.client = client
+
+    def getOutstandingMessages(self):
+        return self.client.getOutstandingMessages()
+
+    def openSocket(self):
+        self.client.getWebSocketMessages(self.messageCallback)
+
+    def messageCallback(self, messageList):
+    #Prcoess/do work with messageList!
+        if(messageList):
+            for message in messageList:
+                #Do work with message here!
+                print("#{}\n{}\n{}".format(message.id, message.title, message.message))
+                if "GBFB Building now DISARMED" in message.title or \
+                    "GBFB Building Status in ALARM" in message.title:
+                        print('Fake emergency shut off initiated')
+                        print('Fake shutting off the arduino now...')
+                        print('Fake shut off completed')
+
+                elif "GBFB Building now ARMED" in message.title and \
+                    "GBFB Building Status Alarm CLEARED" in message.title:
+                        print('Fake resuming normal operations')
+                        print('Fake setting up arduino now...')
+                        print('Fake set up completed')
+
+                #Make sure to acknowledge messages with priority >= 2
+                if(message.priority >= 2):
+                    if(message.acked != 1):
+                        self.client.acknowledgeEmergency(message.receipt)	
+                #Make sure you delete messages that you recieve!
+                self.client.deleteMessages(messageList[-1].id)
 
 def main():
 
     # Setups with a device configuration
-    client = Client("config.json")
+    client = ReyClient(Client("../.secrets/config.json"))
 
     # Get any messages sent before the client has started
     messageList = client.getOutstandingMessages()
 
-    if(messageList):
-        client.deleteMessages(messageList[-1].id)
-
     # Pass our function as a parameter, this will run 'forever'
-    client.getWebSocketMessages(messageCallback)
+    client.openSocket()
 
 if __name__ == "__main__":
     main()
